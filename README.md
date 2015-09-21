21697bcd89b67b798dde120534d54438c46330ac
magnus <Magnusbackyard@live.com>
2015-09-21 21:03:23 +0800

    add toleratedPeriods opt

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 
 Lorem Ipsum generator for Chinese
 It can generate words randomly including punctuation marks in Chinese
+
+中文随机文本生成器
+
 ## Usages
 * sources for testing your word process libs or apps
-* sources for testing your visualized pages or apps  
+* sources for testing your visualized pages or apps
+
+可以用来测试你的中文文字处理库或者程序，也可以用来测试你的可视化页面。
 
 # Installation
 ```shell
@@ -30,9 +35,17 @@ npm install chinesegen
 ```
 
 # Input options
-  - **<u>count</u>**: { _Number_ } the total word count of the generated paragraph
+  - **<u>count</u>**: { _Number_ } the total word count of the generated paragraph 
+
+  篇幅字数总长
+
   - **<u>freq</u>**: { _Boolean_ } apply frequently used words
-  
+  - **<u>toleratedPeriods</u>**: { _String_ } given characters(each represents one period) will be treated as unique periods appended to Chinese periods list
+  ```javascript
+    // treat both Chinese formal periods and '?.!' as full stops of the Chinese sentences
+    generate({count: 50, toleratedPeriods:'?.!'});
+  ```
+
 # Output object
   - **<u>text</u>**: { _String_ } random text
   - **<u>total</u>**: { _Number_ } text total length
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ exports = module.exports = generate;
  * @param {Object} [opts]
  * @param {Number} [opts.count] the total word count of the generated paragraph
  * @param {Boolean} [opts.freq] use frequently used words
+ * @param {String} [opts.format] support '\uXXXX'
+ * @param {String} [opts.toleratedPeriods]
  * @return {Object}
  *  {String} [text] random text
  *  {Number} [total] text total length
@@ -21,15 +23,17 @@ function generate (opts) {
         genCount = Math.abs(options.count) >>> 0,
         // min/max word count in a sentence
         minCount = Math.abs(options.minCount) || 5,
-        maxCount = Math.abs(options.maxCount) || 20;
-
-    var periods = ['\u3002', '\uFF1F', '\uFF01'];
+        maxCount = Math.abs(options.maxCount) || 20,
+        periods = extendPeriod(options.toleratedPeriods || '');
 
     var avail = 0;
-    var genFn = opts.freq === true ? freqUsedWordsGen : wordsGen,
+    var genWords = opts.freq === true ? freqUsedWordsGen : wordsGen,
         wordCountFn = function () {
             var count = rand(minCount, maxCount);
             return count > avail ? avail - 1 : count;
+        },
+        genPeriod = function () {
+            return periods[rand(0, periods.length)];
         },
         genSentences = function (genEachSentenceFn) {
             var gen = {
@@ -49,9 +53,50 @@ function generate (opts) {
     return genSentences(function (gen) {
         var currentWordCount = wordCountFn();
 
-        gen.text += genFn(currentWordCount);
-        gen.text += periods[rand(0, periods.length)];
+        gen.text += genWords(currentWordCount);
+        gen.text += genPeriod();
 
         return currentWordCount + 1;
     });
 }
+
+function extendPeriod (extendString) {
+    var periods = ['\u3002', '\uFF1F', '\uFF01'],
+        lastIndexOf = Array.prototype.lastIndexOf;
+
+    for (var i = 0, eachChar; i < extendString.length; i++) {
+        eachChar = getCharAt.call(extendString, i);
+        if (eachChar === false) continue;
+        // make sure uniqueness
+        if (lastIndexOf.call(periods, eachChar) !== -1) continue;
+
+        periods.push(eachChar);
+    }
+
+    return periods;
+}
+
+// no bound checking
+// return false when unknown character or iterating at the low surrogate
+function getCharAt (index) {
+    var code = this.charCodeAt(index);
+
+    // BMP
+    if (code < 0xD800 || code > 0xDFFF) {
+        return String.fromCharCode(code);
+    }
+
+    // high surrogate
+    if (code < 0xDC00) {
+        // access the low surrogate
+        var nextCode = this.charCodeAt(index + 1);
+        if (isNaN(nextCode)) return false;
+        if (nextCode < 0xDC00 || nextCode > 0xDFFF) return false;
+
+        return String.fromCharCode(code, nextCode);
+    } else {
+        // low surrogate
+        return false;
+    }
+}
+
