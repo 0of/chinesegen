a8dacfb015e937c56b2102d463c5f7199f981070
magnus <Magnusbackyard@live.com>
2015-09-29 20:43:00 +0800

    test cases for format output

--- test/generate.js
+++ test/generate.js
@@ -36,15 +36,24 @@ describe('chinesegen generate API', function () {
 
         should(sentences(/[\u3002\uFF1F\uFF01\.\?\!]/g, generated.text)).exactly(generated.sentenceCount);
     });
+
+    it('should generate corrent word count as it returns when format as \\uXXXX', function () {
+        var format = '\\uXXXX';
+        var total = rand(50, 200);
+        var generated = generate({ count: total, format: format });
+
+        var lens = formatLength(generated.text);
+
+        should(lens.len).exactly(generated.total);
+        should(lens.matched * format.length).exactly(generated.text.length);
+    });
 });
 
 function rand (includedMin, includedMax) {
     return Math.floor(Math.random() * (includedMax - includedMin + 1)) + includedMin;
 }
 
-function isValidChar (index) {
-    var code = this.charCodeAt(index);
-
+function isValidChar (code, next) {
     // BMP
     if (code < 0xD800 || code > 0xDFFF) {
         return true;
@@ -53,7 +62,7 @@ function isValidChar (index) {
     // high surrogate
     if (code < 0xDC00) {
         // access the low surrogate
-        var nextCode = this.charCodeAt(index + 1);
+        var nextCode = next();
         if (isNaN(nextCode)) return false;
         if (nextCode < 0xDC00 || nextCode > 0xDFFF) return false;
 
@@ -67,7 +76,7 @@ function isValidChar (index) {
 function length (text) {
     var len = 0;
     for (var i = 0; i < text.length; i++) {
-        if (isValidChar.call(text, i) === true) len++;
+        if (isValidChar(text.charCodeAt(i), String.prototype.charCodeAt.bind(text, i + 1)) === true) len++;
     }
 
     return len;
@@ -75,4 +84,28 @@ function length (text) {
 
 function sentences (periods, text) {
     return (text.match(periods) || []).length;
+}
+
+function formatLength (text) {
+    var len = 0,
+        matchedCount = 0;
+
+    var exp = /\\u([0-9A-F]{4})/g;
+
+    function goNext () {
+        var matched = exp.exec(text);
+        if (matched) {
+            ++matchedCount;
+            return parseInt(matched[1], 16);
+        }
+
+        return NaN;
+    }
+
+    var current;
+    while (!isNaN(current = goNext())) {
+        if (isValidChar(current, goNext) === true) len++;
+    }
+
+    return {matched: matchedCount, len: len};
 }
